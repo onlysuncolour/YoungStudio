c200cb9f1fc6952dd6de519d6ee1009985dc04e5
刘召阳 <youngliu@thinkingdata.cn>
2024-04-15 15:28:37 +0800

    feat: me pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,20 @@ export default function Home() {
   return (
       <main className={styles.main}>
         <PageDecoration />
-        <div className={styles.meInfo}>
+        <div className={styles.title}>
             我享受生活、玩乐、编程、运动，还有探索
+        </div>
+        <div className={styles.myBlock}>
+          <img src="./me.png" className={styles.mePic} />
+          <div className={styles.meInfo}>
+            <div className={styles.greeting}>
+              G'day mate,
+            </div>
+            <div className={styles.intro}>
+              <p>I'm Young Liu, a front-end developer, located in Shanghai, China.</p>
+              <p>我是刘召阳，一个前端工程师，目前在上海。</p>
+            </div>
+          </div>
         </div>
       </main>
   );
